17940114a5f53b8d48e3cceb213ac9b9ee351c68
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-11-24 22:26:54 +0600

    Refactor

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go_fundamentals/data_types"
-)
-
 func main() {
-	// Numeric Types
-	data_types.SignedIntegers()
-	fmt.Println()
-	data_types.UnsignedIntegers()
-	fmt.Println()
-	data_types.FloatingPoints()
-	fmt.Println()
-	data_types.ComplexNumbers()
-	fmt.Println()
-	data_types.DefaultIntegers()
-	fmt.Println()
-
-	// Boolean Type
-	data_types.BooleanData()
-	fmt.Println()
-
-	// String Type
-	data_types.StringData()
+	RunLesson(ConstantDataTypes)
 }
---
+++ utils.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"go_fundamentals/data_types"
+)
+
+const (
+	BooleanDataTypes  = "BOOLEAN_DATA_TYPES"
+	ConstantDataTypes = "CONSTANT_DATA_TYPES"
+	NumericDataTypes  = "NUMERIC_DATA_TYPES"
+	StringDataTypes   = "STRING_DATA_TYPES"
+)
+
+func RunLesson(lesson string) {
+	switch lesson {
+	case NumericDataTypes:
+		// Numeric Types
+		data_types.SignedIntegers()
+		fmt.Println()
+		data_types.UnsignedIntegers()
+		fmt.Println()
+		data_types.FloatingPoints()
+		fmt.Println()
+		data_types.ComplexNumbers()
+		fmt.Println()
+		data_types.DefaultIntegers()
+		fmt.Println()
+	case BooleanDataTypes:
+		// Boolean Type
+		data_types.BooleanData()
+		fmt.Println()
+	case StringDataTypes:
+		// String Type
+		data_types.StringData()
+		fmt.Println()
+	case ConstantDataTypes:
+		// Constants
+		data_types.Constants()
+		fmt.Println()
+	}
+
+}
